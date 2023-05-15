50611efc9127b07e46eefff3f5bbcf1448565392
Seunghyun-FE <glacks149@gmail.com>
2023-05-15 17:56:04 +0900

    + page, components

---
+++ my-app/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function footer() {
+  
+    return (
+        <footer>
+            <span className='footer_content'>개인정보 처리방침|이용 약관</span>
+            <span className='footer_content'>All rights reserved @ Codestates</span>
+        </footer>
+        
+    )
+}
---
+++ my-app/src/pages/MainPages.js
@@ -0,0 +1,11 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+const Main = () => {
+    return (
+        <div>
+            <Header/>
+            <Footer/>
+        </div>
+    )
+}
